baaa72d8d5b63a62e80280d4afa8f9f3470933cd
Jason Rodgers <ja_rod@hotmail.com>
2022-01-03 21:10:07 -0600

    add No Inventory sentence temporarily remove the sound options for now

--- TheMusicIIMathExperience/src/app/home/home.page.ts
+++ TheMusicIIMathExperience/src/app/home/home.page.ts
@@ -14,17 +14,18 @@ export class HomePage {
     private platform: Platform) {}
 
   startWork(pagePath:string){
-    if (this.platform.is('cordova')) {
-      this.nativeAudio.preloadSimple('transitionSound', '../../assets/sounds/Chimes-sound-effect.mp3')
-      .then(()=>{
-        this.nativeAudio.play('transitionSound').then(()=>{
-          this.navCtrl.navigateForward('/' + pagePath);
-        });
+    // if (this.platform.is('cordova')) {
+    //   this.nativeAudio.preloadSimple('transitionSound', '../../assets/sounds/Chimes-sound-effect.mp3')
+    //   .then(()=>{
+    //     this.nativeAudio.play('transitionSound').then(()=>{
+    //       this.navCtrl.navigateForward('/' + pagePath);
+    //     });
 
-      });
-    }
-    else{
-      this.navCtrl.navigateForward('/' + pagePath);
-    }
+    //   });
+    // }
+    // else{
+    //   this.navCtrl.navigateForward('/' + pagePath);
+    // }
+    this.navCtrl.navigateForward('/' + pagePath);
   }
 }
